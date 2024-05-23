5e7c82ee7fc02cd0f5d7881fae641baaa404fc4e
Crypto Lisboa <dey00tsnetwork@gmail.com>
2024-05-23 22:54:50 +0100

    Refactor SkinBuilder application files, remove unnecessary scripts

---
+++ scripts/imageUploader.ts
@@ -0,0 +1,12 @@
+// https://www.youtube.com/watch?v=wyYkpMgEVxE
+// npx thirdweb@lastest upload <path> // works for folders too
+
+/*
+✔ Files stored at the following IPFS URI: ipfs://QmepVtdMMd9APQ6qwbUTyLHfkBD7x2ePRqPNyvMPwze2xV
+✔ Open this link to view your upload: https://bafybeihu3veecxn5bscv3jrenittgviqkbe4epek6poggozbbdsx2vm6jy.ipfs.cf-ipfs.com/
+*/
+// import { upload } from 'thirdweb/storage'
+// const uri = await upload({
+//   client,
+//   files: [new File(['hello world'], 'hello.txt')],
+// })
