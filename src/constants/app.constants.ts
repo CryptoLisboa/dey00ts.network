489eb7b49aaf00bb95b0ff837252f2be07c543e2
Crypto Lisboa <dey00tsnetwork@gmail.com>
2024-05-30 18:26:31 +0100

    feat: Add id field to Skill type in app.constants.ts
    The 'id' field has been added to the Skill type in the app.constants.ts file. This change ensures that each skill object has a unique identifier, which can be useful for various operations and data management.

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -2,18 +2,19 @@
 export type Skill = {
   name: string
   color: string
+  id: number
 }
 
 // define the constant array of skills
 export const SKILLS: Skill[] = [
-  { name: 'Alpha Caller', color: '#BD8BFF' },
-  { name: 'Collab Manager', color: '#06F0FF' },
-  { name: 'Community Builder', color: '#FF6B6B' },
-  { name: 'Sol Dev', color: '#FFC9DF' },
-  { name: 'Content Creator', color: '#1BCEA3' },
-  { name: 'EVM Dev', color: '#FFFF75' },
-  { name: 'Full Stack', color: '#FFA500' },
-  { name: 'Space Host', color: '#3792FF' },
+  { name: 'Alpha Caller', color: '#BD8BFF', id: 1 },
+  { name: 'Collab Manager', color: '#06F0FF', id: 2 },
+  { name: 'Community Builder', color: '#FF6B6B', id: 3 },
+  { name: 'Sol Dev', color: '#FFC9DF', id: 4 },
+  { name: 'Content Creator', color: '#1BCEA3', id: 5 },
+  { name: 'EVM Dev', color: '#FFFF75', id: 6 },
+  { name: 'Full Stack', color: '#FFA500', id: 7 },
+  { name: 'Space Host', color: '#3792FF', id: 8 },
 ]
 
 // define a type for the skill names
