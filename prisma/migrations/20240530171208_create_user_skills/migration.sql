6b0298be844fefa1e051bfbdfcd4e8a33ae337e6
Crypto Lisboa <dey00tsnetwork@gmail.com>
2024-05-30 18:12:36 +0100

    feat: Add migration for creating Skill and UserSkills tables
    This commit adds a migration file to create the Skill and UserSkills tables in the database. The Skill table has columns for 'id' and 'name', with the 'id' column set as the primary key. The UserSkills table has columns for 'A' and 'B', which are foreign keys referencing the 'id' columns of the Skill and User tables respectively. Indexes and constraints are also added to ensure data integrity.

---
+++ prisma/migrations/20240530171208_create_user_skills/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Skill" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Skill_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_UserSkills" (
+    "A" INTEGER NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Skill_name_key" ON "Skill"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserSkills_AB_unique" ON "_UserSkills"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserSkills_B_index" ON "_UserSkills"("B");
+
+-- AddForeignKey
+ALTER TABLE "_UserSkills" ADD CONSTRAINT "_UserSkills_A_fkey" FOREIGN KEY ("A") REFERENCES "Skill"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserSkills" ADD CONSTRAINT "_UserSkills_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
