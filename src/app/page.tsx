53f569c8ad1cfa5129b2271383999cdf8c6b99f9
Crypto Lisboa <dey00tsnetwork@gmail.com>
2024-06-02 13:04:04 +0100

    Refactor page.tsx to remove unused code and improve performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { auth } from '@/auth'
 import { Home } from '@/app/Home.client'
 
 export default async function HomePage() {
-  const session = await auth()
   return (
     <div className='dark' id='root'>
-      <Home isAuthenticated={!!session} />
+      <Home />
     </div>
   )
 }
