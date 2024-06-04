403b4adc5f52838cb736aa9aaf3daa3d5593fbf2
joliveira-futsphere <150056326+joliveira-futsphere@users.noreply.github.com>
2024-06-04 16:11:58 +0100

    Add dark map
    Add inital avatar marker
    Add overlay for selected user

--- src/app/map/MapView.tsx
+++ src/app/map/MapView.tsx
@@ -1,17 +1,69 @@
 'use client'
 
 import { Spinner } from '@nextui-org/react'
-import { LatLngExpression } from 'leaflet'
-import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { LatLng, LatLngExpression } from 'leaflet'
+import React, { useLayoutEffect, useState } from 'react'
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from 'react-leaflet'
+import L from 'leaflet'
+import { useSession } from 'next-auth/react'
 
 const defaultCenter: LatLngExpression = [38.9072, -77.0369]
 const defaultZoom = 8
 
+function LocationMarker() {
+  const session = useSession()
+  const [position, setPosition] = useState<LatLng | null>(null)
+  const map = useMapEvents({
+    click() {
+      map.locate()
+    },
+
+    locationfound(e) {
+      debugger
+      setPosition(e.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+  })
+
+  const myIcon = L.icon({
+    iconUrl: session.data?.user.image || '/temp/avatar.png',
+    iconSize: [64, 64],
+    iconAnchor: [32, 64],
+    className: 'rounded-full',
+    // popupAnchor: null,
+    // shadowUrl: null,
+    // shadowSize: null,
+    // shadowAnchor: null,
+  })
+
+  console.log({ position })
+  return position === null ? null : (
+    <Marker position={position} icon={myIcon}>
+      <Popup>You are here</Popup>
+    </Marker>
+  )
+}
+
 export const MapView = () => {
-  if (typeof window === 'undefined') {
+  const [unmountMap, setunmountMap] = useState(false)
+
+  useLayoutEffect(() => {
+    setunmountMap(false)
+    return () => {
+      setunmountMap(true)
+    }
+  }, [])
+
+  if (typeof window === 'undefined' || unmountMap) {
     return <Spinner />
   }
+
   return (
     <MapContainer
       center={defaultCenter}
@@ -22,6 +74,7 @@ export const MapView = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
+      <LocationMarker />
     </MapContainer>
   )
 }
--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Chip, Image, Input } from '@nextui-org/react'
+import NextImage from 'next/image'
 import dynamic from 'next/dynamic'
 
 const DynamicMapView = dynamic(
@@ -9,7 +11,70 @@ const DynamicMapView = dynamic(
 
 export default function Page() {
   return (
-    <main className='dark flex w-full h-[85vh]' id='map'>
+    <main className='dark flex w-full h-[85vh] relative' id='map'>
+      <div
+        className='flex flex-col gap-3 w-[200px] h-[200px] md:w-[500px] md:h-[500px]'
+        style={{
+          position: 'absolute',
+          // width: 500,
+          // height: 500,
+          top: 10,
+          left: 60,
+          zIndex: 10000,
+        }}
+      >
+        <Input
+          type='location'
+          placeholder='Search by: Interest, location, language'
+          labelPlacement='outside'
+          className='w-full'
+          startContent={
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='size-6'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z'
+              />
+            </svg>
+          }
+        />
+
+        <div className='flex gap-3'>
+          <Chip variant='shadow' color='success'>
+            DeGods
+          </Chip>
+          <Chip variant='shadow' color='warning'>
+            Y00ts
+          </Chip>
+          <Chip variant='shadow' color='primary'>
+            BTC DeGods
+          </Chip>
+        </div>
+
+        {true && (
+          <div className='flex gap-3 bg-white rounded-xl w-1/2 justify-around p-3'>
+            <Image
+              src={'temp/avatar.png'}
+              alt={'Testing'}
+              width={50}
+              height={50}
+              className='rounded-full'
+            />
+
+            <div className='flex flex-col'>
+              <span className='font-lucky'>username</span>
+              <span className='font-lucky'>location</span>
+            </div>
+          </div>
+        )}
+      </div>
       <DynamicMapView />
     </main>
   )
