f418246b07aa1fa9920bb0a918fa82c2582fa9ef
cryptolisboa <ben_hur_cf@icloud.com>
2024-08-13 03:16:13 +0100

    Add `react-device-detect` and update UserList layout
    - Added `react-device-detect` dependency to allow detection of device type in the frontend
    - Refined the `UserList` component layout by switching to CSS grid, enhancing responsiveness
    - Introduced utility function to standardize skill styles
    - Minor cleanup and commented out unused image imports for better readability
    
    These updates aim to improve layout aesthetics and future-proof the codebase with device detection capabilities.

--- src/app/app/UserList.tsx
+++ src/app/app/UserList.tsx
@@ -1,6 +1,15 @@
-import { Image } from '@nextui-org/react'
-import NextImage from 'next/image'
+// import { Image } from '@nextui-org/react'
+// import NextImage from 'next/image'
+import Image from 'next/image'
 import Link from 'next/link'
+
+const getSkillStyles = (skillName: string) => {
+  const formattedSkillName = skillName.toLowerCase().replace(/ /g, '-')
+  return {
+    color: `var(--color-${formattedSkillName})`,
+    borderColor: `var(--color-${formattedSkillName})`,
+  }
+}
 
 export const UserList = ({
   users,
@@ -10,7 +19,10 @@ export const UserList = ({
   className: string
 }) => {
   return (
-    <div className={className}>
+    <div
+      // className={className}
+      className='grid grid-cols-1 lg:grid-cols-2 gap-3 w-full mb-4'
+    >
       {users?.map((user: any) => {
         if (user?.socials?.twitterHandle && user?.name) {
           return (
@@ -20,39 +32,31 @@ export const UserList = ({
             >
               <Link
                 href={`/${user?.socials?.twitterHandle}`}
-                className='flex items-center gap-1 w-full h-full'
+                className='grid grid-cols-12 items-center gap-0.5 lg:gap-1 w-full h-full'
                 prefetch={true}
               >
                 {user?.image && (
-                  <Image
-                    as={NextImage}
-                    className='rounded-lg'
-                    src={user?.image}
-                    width={64}
-                    height={64}
-                    alt='avatar'
-                    unoptimized
-                  />
+                  <div className='w-16 h-full bg-blue-200 col-span-3'>
+                    <Image
+                      className='rounded-lg'
+                      src='https://pbs.twimg.com/profile_images/1821304671081738240/qeI3FyEi_400x400.jpg'
+                      width={64}
+                      height={64}
+                      alt='avatar'
+                      unoptimized
+                    />
+                  </div>
                 )}
 
-                <div className='flex flex-col h-full justify-between py-2'>
-                  <strong
-                    style={{
-                      color: `var(--color-${user?.skills[0]?.name
-                        .toLowerCase()
-                        .replace(/ /g, '-')})`,
-                      borderColor: `var(--color-${user?.skills[0]?.name
-                        .toLowerCase()
-                        .replace(/ /g, '-')})`,
-                    }}
-                  >
+                <div className='flex flex-col h-full justify-between py-2 col-span-6 lg:text-base text-sm'>
+                  <strong style={getSkillStyles(user?.skills[0]?.name)}>
                     {user?.skills[0]?.name}
                   </strong>
                   <p>@{user?.socials?.twitterHandle}</p>
                 </div>
 
                 {!!user?.location?.name && (
-                  <div className='flex ml-auto pr-3 text-right'>
+                  <div className='text-wrap overflow-clip text-ellipsis h-full col-span-3 lg:text-base text-sm flex flex-col justify-end py-2'>
                     {user?.location?.name}
                   </div>
                 )}
--- src/app/app/UserSearchAndList.client.tsx
+++ src/app/app/UserSearchAndList.client.tsx
@@ -80,7 +80,7 @@ export const UserSearchAndList = ({
           ))}
         </div>
       </div>
-      <UserList className='flex flex-col gap-3 w-full mb-4 ' users={users} />
+      <UserList className='flex flex-col gap-3 w-full mb-4' users={users} />
 
       {/* pagination buttons */}
       <div className='flex justify-center gap-x-4'>
