705dfd08900697f00fe839892142304613bf9e3e
cryptolisboa <ben_hur_cf@icloud.com>
2024-08-13 04:19:20 +0100

    Improve UserList layout for better responsiveness
    - Enhanced grid layout to use 4 columns on large screens, optimizing the display of user cards.
    - Updated user card structure to use a more flexible and responsive flex and grid combination.
    - Increased image size in user cards for better visibility and user emphasis.
    - Improved text wrapping and overflow handling to better display user details without layout issues.

--- src/app/app/UserList.tsx
+++ src/app/app/UserList.tsx
@@ -19,47 +19,53 @@ export const UserList = ({
   className: string
 }) => {
   return (
-    <div
-      // className={className}
-      className='grid grid-cols-1 lg:grid-cols-2 gap-3 w-full mb-4'
-    >
+    <div className='grid grid-cols-1 lg:grid-cols-4 gap-3 w-full mb-4'>
       {users?.map((user: any) => {
         if (user?.socials?.twitterHandle && user?.name) {
           return (
-            <div
-              key={user?.id}
-              className='flex w-full items-center justify-between h-16'
-            >
+            <div key={user?.id} className='flex flex-col lg:gap-2'>
               <Link
                 href={`/${user?.socials?.twitterHandle}`}
-                className='grid grid-cols-12 items-center gap-0.5 lg:gap-1 w-full h-full'
+                className='grid grid-cols-12 items-center gap-0.5 lg:grid-cols-1 lg:gap-2 w-full lg:w-72 h-full'
                 prefetch={true}
               >
                 {user?.image && (
-                  <div className='w-16 h-full bg-blue-200 col-span-3'>
-                    <Image
-                      className='rounded-lg'
-                      src='https://pbs.twimg.com/profile_images/1821304671081738240/qeI3FyEi_400x400.jpg'
-                      width={64}
-                      height={64}
-                      alt='avatar'
-                      unoptimized
-                    />
+                  <div className='w-16 h-16 col-span-3 lg:w-72 lg:h-auto lg:flex lg:justify-center'>
+                    <div className='lg:w-72 lg:h-72'>
+                      <Image
+                        className='rounded-lg w-full h-full object-cover'
+                        src={
+                          user?.image ||
+                          'https://pbs.twimg.com/profile_images/1821304671081738240/qeI3FyEi_400x400.jpg'
+                        }
+                        width={128}
+                        height={128}
+                        alt={`${user?.name}`}
+                        unoptimized
+                      />
+                    </div>
                   </div>
                 )}
 
-                <div className='flex flex-col h-full justify-between py-2 col-span-6 lg:text-base text-sm'>
-                  <strong style={getSkillStyles(user?.skills[0]?.name)}>
-                    {user?.skills[0]?.name}
-                  </strong>
-                  <p>@{user?.socials?.twitterHandle}</p>
+                <div className='flex lg:flex-row lg:justify-between lg:w-full items-center col-span-9 h-full gap-x-2'>
+                  <div className='flex flex-col justify-between py-2 lg:py-0'>
+                    <strong
+                      className='text-wrap overflow-clip text-ellipsis lg:text-sm text-xs'
+                      style={getSkillStyles(user?.skills[0]?.name)}
+                    >
+                      {user?.skills[0]?.name}
+                    </strong>
+                    <p className='text-wrap overflow-clip text-ellipsis lg:text-sm text-xs'>
+                      @{user?.socials?.twitterHandle}
+                    </p>
+                  </div>
+
+                  {!!user?.location?.name && (
+                    <div className='text-wrap overflow-clip text-ellipsis lg:text-sm text-xs text-right'>
+                      {user?.location?.name}
+                    </div>
+                  )}
                 </div>
-
-                {!!user?.location?.name && (
-                  <div className='text-wrap overflow-clip text-ellipsis h-full col-span-3 lg:text-base text-xs flex flex-col justify-end py-2'>
-                    {user?.location?.name}
-                  </div>
-                )}
               </Link>
             </div>
           )
