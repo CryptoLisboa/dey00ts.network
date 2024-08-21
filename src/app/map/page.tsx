40aa88fc4caf43222c0da35feedb3631cb9064ed
cryptolisboa <ben_hur_cf@icloud.com>
2024-08-21 02:26:13 +0100

    Temporarily disable unused Chip component display
    Commented out the import and DOM usage of the `Chip` component from `@nextui-org/react`. This clears up unused code, which may be part of ongoing UI adjustments or simplifications. Helps to maintain clean code and ensure unnecessary elements do not affect the layout or performance

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Chip } from '@nextui-org/react'
+// import { Chip } from '@nextui-org/react'
 import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import useSWR from 'swr'
@@ -21,7 +21,7 @@ export default function Page() {
       className='dark flex flex-col gap-y-3 w-full h-[85vh] relative'
       id='map'
     >
-      <div
+      {/* <div
         className='flex flex-col gap-3 w-fit h-fit'
         style={{
           top: 10,
@@ -40,7 +40,7 @@ export default function Page() {
             BTC DeGods
           </Chip>
         </div>
-      </div>
+      </div> */}
       <DynamicMapView
         users={data}
         handleClick={setSelectedUser}
